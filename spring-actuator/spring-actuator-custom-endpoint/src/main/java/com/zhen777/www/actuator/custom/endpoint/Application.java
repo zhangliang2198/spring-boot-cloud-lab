e381a47cfa047e665154e0fdddd5d2a39b456c3b
张亮 <zhangl02@szlanyou.com>
2023-02-23 18:03:06 +0800

    -- done

---
+++ spring-actuator/spring-actuator-custom-endpoint/src/main/java/com/zhen777/www/actuator/custom/endpoint/Application.java
@@ -0,0 +1,13 @@
+package com.zhen777.www.actuator.custom.endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
