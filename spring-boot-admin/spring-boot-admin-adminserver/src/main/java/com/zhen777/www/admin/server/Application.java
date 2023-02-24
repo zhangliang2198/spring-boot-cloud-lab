0d4221eb5b9692e3cd3bb7d4ca012076ac421145
mikoozhang <48906088@qq.com>
2023-02-24 09:38:18 +0800

    -- spring boot admin init

---
+++ spring-boot-admin/spring-boot-admin-adminserver/src/main/java/com/zhen777/www/admin/server/Application.java
@@ -0,0 +1,15 @@
+package com.zhen777.www.admin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
