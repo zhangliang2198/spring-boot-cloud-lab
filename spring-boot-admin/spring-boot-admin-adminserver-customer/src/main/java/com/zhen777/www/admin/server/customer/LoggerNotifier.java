bd23ca3b6f5cd624247105c10235136c3c79978a
mikoozhang <48906088@qq.com>
2023-02-24 11:23:00 +0800

    -- spring boot admin  warn

---
+++ spring-boot-admin/spring-boot-admin-adminserver-customer/src/main/java/com/zhen777/www/admin/server/customer/LoggerNotifier.java
@@ -0,0 +1,34 @@
+package com.zhen777.www.admin.server.customer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import de.codecentric.boot.admin.server.domain.entities.Instance;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
+import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
+import de.codecentric.boot.admin.server.notify.AbstractStatusChangeNotifier;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggerNotifier extends AbstractStatusChangeNotifier {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public LoggerNotifier(InstanceRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    protected Mono<Void> doNotify(InstanceEvent event, Instance instance) {
+        return Mono.fromRunnable(() -> {
+            if (event instanceof InstanceStatusChangedEvent) {
+                logger.info("Instance {} ({}) is {}", instance.getRegistration().getName(), event.getInstance(),
+                        ((InstanceStatusChangedEvent) event).getStatusInfo().getStatus());
+            } else {
+                logger.info("Instance {} ({}) {}", instance.getRegistration().getName(), event.getInstance(), event.getType());
+            }
+        });
+    }
+
+}
