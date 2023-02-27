78141fa606ad32d4f94664d1f9436d79f91500b7
mikoozhang <48906088@qq.com>
2023-02-27 12:27:59 +0800

    -- 滚动日志

--- spring-boot-admin/spring-boot-admin-demoapplication-byheader/src/main/java/com/zhen777/www/admin/demoapplication/byheader/Application.java
+++ spring-boot-admin/spring-boot-admin-demoapplication-byheader/src/main/java/com/zhen777/www/admin/demoapplication/byheader/Application.java
@@ -3,12 +3,27 @@ package com.zhen777.www.admin.demoapplication.byheader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    private static void doLog() {
+        log.info("日志:" + LocalDateTime.now());
     }
 
 }
