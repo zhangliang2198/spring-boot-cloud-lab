da88dd4ffdc6818fb134acaa17a9bc35676bcf7f
mikoozhang <48906088@qq.com>
2023-03-01 22:17:07 +0800

    -- lab00

---
+++ lab00-spring-boot-base/lab00-spring-boot-starter-customer-server/src/main/java/com/zhen777/www/customer/starter/server/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhen777.www.customer.starter.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
