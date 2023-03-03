b8c225ee1785c505a5848d831208609c50a04b02
mikoozhang <48906088@qq.com>
2023-03-03 20:23:51 +0800

    -- 处理注解

---
+++ lab02-spring-boot-config/config-base/src/main/java/com/zhen777/www/config/base/config/OrderProperties.java
@@ -0,0 +1,22 @@
+package com.zhen777.www.config.base.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "order")
+public class OrderProperties {
+
+    /**
+     * 订单支付超时时长，单位：秒。
+     */
+    private Integer payTimeoutSeconds;
+
+    /**
+     * 订单创建频率，单位：秒
+     */
+    private Integer createFrequencySeconds;
+
+    // ... 省略 set/get 方法
+
+}
