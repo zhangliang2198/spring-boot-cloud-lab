ce962d89b9652ea68d6522bfc8c8ab6253f8da40
mikoozhang <48906088@qq.com>
2023-03-03 21:08:39 +0800

    -- 处理注解

---
+++ lab02-spring-boot-config/lab02-config-with-env/src/main/java/com/zhen777/www/config/base/ProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.zhen777.www.config.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProfilesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilesApplication.class, args);
+    }
+
+}
