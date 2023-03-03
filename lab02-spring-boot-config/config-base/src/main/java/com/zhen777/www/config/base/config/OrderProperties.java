ebd123fece81fa159a6ac866bb24498128f906a4
mikoozhang <48906088@qq.com>
2023-03-03 20:50:16 +0800

    -- 处理注解

--- lab02-spring-boot-config/config-base/src/main/java/com/zhen777/www/config/base/config/OrderProperties.java
+++ lab02-spring-boot-config/config-base/src/main/java/com/zhen777/www/config/base/config/OrderProperties.java
@@ -1,10 +1,12 @@
 package com.zhen777.www.config.base.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "order")
+@Data
 public class OrderProperties {
 
     /**
