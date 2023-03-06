a74b20def9612f57695d311b8bb111d3747b0842
mikoozhang <48906088@qq.com>
2023-03-06 15:43:50 +0800

    -- 搭建测试多数据源的代码

--- lab-17-dynamic-datasource-baomidou/lab-17-dynamic-datasource-baomidou-01/src/main/java/com/zhen777/www/dynamic/datasource/baomidou/service/OrderService.java
+++ lab-17-dynamic-datasource-baomidou/lab-17-dynamic-datasource-baomidou-01/src/main/java/com/zhen777/www/dynamic/datasource/baomidou/service/OrderService.java
@@ -4,11 +4,14 @@ package com.zhen777.www.dynamic.datasource.baomidou.service;
 
 import com.baomidou.dynamic.datasource.annotation.DS;
 import com.zhen777.www.dynamic.datasource.baomidou.constant.DBConstants;
+import com.zhen777.www.dynamic.datasource.baomidou.dataobject.OrderDO;
+import com.zhen777.www.dynamic.datasource.baomidou.dataobject.UserDO;
 import com.zhen777.www.dynamic.datasource.baomidou.mapper.OrderMapper;
 import com.zhen777.www.dynamic.datasource.baomidou.mapper.UserMapper;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -24,26 +27,79 @@ public class OrderService {
     }
 
     public void method01() {
-        // ... 省略代码
+        // 查询订单
+        OrderDO order = orderMapper.selectById(1);
+        System.out.println(order);
+        // 查询用户
+        UserDO user = userMapper.selectById(1);
+        System.out.println(user);
     }
 
     @Transactional
     public void method02() {
-        // ... 省略代码
+        // 查询订单
+        OrderDO order = orderMapper.selectById(1);
+        System.out.println(order);
+        // 查询用户
+        UserDO user = userMapper.selectById(1);
+        System.out.println(user);
     }
 
     public void method03() {
-        // ... 省略代码
+        // 查询订单
+        self().method031();
+        // 查询用户
+        self().method032();
     }
 
+    @Transactional // 报错，因为此时获取的是 primary 对应的 DataSource ，即 users 。
+    public void method031() {
+        OrderDO order = orderMapper.selectById(1);
+        System.out.println(order);
+    }
+
+    @Transactional
+    public void method032() {
+        UserDO user = userMapper.selectById(1);
+        System.out.println(user);
+    }
+
+// OrderService.java
+
     public void method04() {
-        // ... 省略代码
+        // 查询订单
+        self().method041();
+        // 查询用户
+        self().method042();
     }
 
     @Transactional
     @DS(DBConstants.DATASOURCE_ORDERS)
-    public void method05() {
-        // ... 省略代码
+    public void method041() {
+        OrderDO order = orderMapper.selectById(1);
+        System.out.println(order);
     }
 
+    @Transactional
+    @DS(DBConstants.DATASOURCE_USERS)
+    public void method042() {
+        UserDO user = userMapper.selectById(1);
+        System.out.println(user);
+    }
+    @Transactional
+    @DS(DBConstants.DATASOURCE_ORDERS)
+    public void method05() {
+        // 查询订单
+        OrderDO order = orderMapper.selectById(1);
+        System.out.println(order);
+        // 查询用户
+        self().method052();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @DS(DBConstants.DATASOURCE_USERS)
+    public void method052() {
+        UserDO user = userMapper.selectById(1);
+        System.out.println(user);
+    }
 }
