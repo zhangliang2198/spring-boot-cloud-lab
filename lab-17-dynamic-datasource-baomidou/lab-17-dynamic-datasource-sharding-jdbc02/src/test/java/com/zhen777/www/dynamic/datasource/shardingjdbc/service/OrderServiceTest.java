960803d4e8490a246b43a6995d27f6f26d5d2c18
mikoozhang <48906088@qq.com>
2023-03-07 12:33:10 +0800

    -- shard 多数据源

---
+++ lab-17-dynamic-datasource-baomidou/lab-17-dynamic-datasource-sharding-jdbc02/src/test/java/com/zhen777/www/dynamic/datasource/shardingjdbc/service/OrderServiceTest.java
@@ -0,0 +1,32 @@
+package com.zhen777.www.dynamic.datasource.shardingjdbc.service;
+
+
+import com.zhen777.www.dynamic.datasource.shardingjdbc.Application;
+import com.zhen777.www.dynamic.datasource.shardingjdbc.dataobject.OrderDO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Test
+    public void testAdd() {
+        OrderDO order = new OrderDO();
+        order.setUserId(20);
+        orderService.add(order);
+    }
+
+    @Test
+    public void testFindById() {
+        OrderDO order = orderService.findById(1);
+        System.out.println(order);
+    }
+
+}
