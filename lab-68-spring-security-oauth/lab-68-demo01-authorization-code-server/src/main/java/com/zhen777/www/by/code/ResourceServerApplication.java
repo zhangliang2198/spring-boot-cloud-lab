d560f9d1e10ea0c510d93b0e8d6d175f2cf400ed
mikoozhang <48906088@qq.com>
2023-03-10 20:12:04 +0800

    -- oauth

---
+++ lab-68-spring-security-oauth/lab-68-demo01-authorization-code-server/src/main/java/com/zhen777/www/by/code/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.zhen777.www.by.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+
+}
