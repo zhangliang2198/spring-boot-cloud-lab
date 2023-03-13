f2dd3703003441f4e6bc504f02231d3c48bebd9e
mikoozhang <48906088@qq.com>
2023-03-13 10:15:11 +0800

    -- oauth sso

---
+++ lab-68-spring-security-oauth/lab-68-demo21-authorization-server-on-sso/src/main/java/com/zhen777/www/server/on/sso/AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.zhen777.www.server.on.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ lab-68-spring-security-oauth/lab-68-demo21-resource-server-on-sso/src/main/java/com/zhen777/www/on/sso/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.zhen777.www.on.sso.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户 Controller
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/info")
+    public Authentication info(Authentication authentication) {
+        return authentication;
+    }
+
+}
